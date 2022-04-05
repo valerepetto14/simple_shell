257976f8d86f1a79deaaf3f4d5623e52b1291f57
MateoArbini <mateoarbini1998@gmail.com>
2022-04-05 15:18:21 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#define BLANCO_T   "\x1b[37m"
+#define VERDE_T        "\x1b[32m"
+
+int ContTokens(char *cadena1);
+
+#endif
